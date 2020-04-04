7e291068f9e46973af47fb7527e4eec37fc12039
Ethan Tang <ethanyidong@gmail.com>
2020-04-04 12:24:06 -0700

    Removed unnessesary components

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,47 @@
-#![recursion_limit = "512"]
-
-mod app;
-mod utils;
-
+use yew::prelude::*;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+struct App {
+    clicked: bool,
+    link: ComponentLink<Self>,
+}
 
-// This is the entry point for the web app
+enum Msg {
+    Click,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        App {
+            clicked: false,
+            link,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click => {
+                self.clicked = true;
+                true // Indicate that the Component should re-render
+            }
+        }
+    }
+
+    fn view(&self) -> Html {
+        let button_text = if self.clicked { "Clicked!" } else { "Click me!" };
+
+        html! {
+            <button onclick=self.link.callback(|_| Msg::Click)>{ button_text }</button>
+        }
+    }
+}
+
 #[wasm_bindgen]
 pub fn run_app() -> Result<(), JsValue> {
-    utils::set_panic_hook();
-    web_logger::init();
-    yew::start_app::<app::App>();
+    yew::start_app::<App>();
+
     Ok(())
 }
